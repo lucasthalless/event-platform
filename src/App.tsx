e5d902b3d8cee9a6b05d99c650f70f29788fe8af
lucas <lucas.tha.santos@gmail.com>
2022-06-25 13:53:40 -0300

    feat: subscription via graphql (fourth class)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { ApolloProvider } from "@apollo/client";
-
-import { Event } from "./pages/Event";
-import { client } from "./lib/apollo";
 import { BrowserRouter } from "react-router-dom";
+
+import { client } from "./lib/apollo";
 import { Router } from "./Router";
 
 function App() {
